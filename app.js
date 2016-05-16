4f8945c13decd352c951e1ad2d9e954397915509
TR <anthony.mcalexander@gmail.com>
2016-05-16 10:04:57 -0400

    Checking in

--- app.js
+++ app.js
@@ -3,12 +3,7 @@
 const DigitalOceanHelper = require('./lib/do_class');
 var item = new DigitalOceanHelper();
 
-item.create()
 item.accountGetActions({},function(err,response, body){
   console.log(body)
 });
-// .then((status) =>{
-//   console.log(status);
-// })
-
 
--- test/test.js
+++ test/test.js
@@ -4,15 +4,15 @@ const expect = require("chai").expect;
 var DO_OBJ = require("../lib/do_class");
 var do_obj = new DO_OBJ();
 
-describe('Check Account call', function(){
-  it('checks account status is good', (done)=>{
-    do_obj.account_data()
-    .then((data)=>{
-      expect(data.account.status).to.equal('active');
-      done();
-    })
-    .catch((err)=>{
-      done(err);
-    })
-  });
-})
+// describe('Check Account call', function(){
+//   it('checks account status is good', (done)=>{
+//     do_obj.account_data()
+//     .then((data)=>{
+//       expect(data.account.status).to.equal('active');
+//       done();
+//     })
+//     .catch((err)=>{
+//       done(err);
+//     })
+//   });
+// })
