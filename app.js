9880f3490ca8272948462b288da657f0dbff919c
TR <anthony.mcalexander@gmail.com>
2016-05-16 10:04:23 -0400

    Moving to a helper class

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 'use strict'
 
-const DO_CLASS = require('./lib/do_class');
+const DigitalOceanHelper = require('./lib/do_class');
+var item = new DigitalOceanHelper();
 
-var item = new DO_CLASS();
-item.account_data()
-.then((data) => {
-  return item.validate_data(data);
-})
-.then((status) =>{
-  console.log(status);
-})
-.catch((err)=>{
-  console.dir("ERROR!");
-  console.dir(err);
-})
+item.create()
+item.accountGetActions({},function(err,response, body){
+  console.log(body)
+});
+// .then((status) =>{
+//   console.log(status);
+// })
+
 
