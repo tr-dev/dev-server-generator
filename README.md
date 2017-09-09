4319a13af10be3ce82d9fceb1bfea2058d5f7f06
tr <tr@mcalexander.me>
2017-09-09 15:25:33 -0400

    Adding ssh key generation

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dev Server Generator
+
+Generates keys, adds them to your Digital Ocean Account, and Launches
+
+## TODO
+- Add functionality to launch droplet
+- Run in Docker container
+- Add ansible code to provision server
+
--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 'use strict'
 
-const DigitalOceanHelper = require('./lib/do_class');
-var item = new DigitalOceanHelper();
+const DigitalOceanHelper = require('./lib/DigitalOceanHelper');
+const DO_KEY    = process.env.DO_KEY;
+const PAGE_SIZE = process.env.PAGE_SIZE || 2;
+const DOHelper = new DigitalOceanHelper(DO_KEY, PAGE_SIZE);
 
-item.accountGetActions({},function(err,response, body){
-  console.log(body)
-});
 
+DOHelper.generateKeys()
+.then((results) => {
+  
+  return DOHelper.accountAddKey({
+    name: results.uuid,
+    public_key: results.public_key
+  })
+}, (err) =>{ 
+  console.log(`Error generating keys: ${err}`)
+
+})
+.then((results)=>{
+  console.log(results.body);
+}, (err) => {
+  console.log(err)
+})
