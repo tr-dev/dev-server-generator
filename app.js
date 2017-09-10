c450279761dd29a4828d2f35606dfba6128720d4
tr <tr@mcalexander.me>
2017-09-10 15:14:37 -0400

    Adding CLI interface for managing ssh keys

--- app.js
+++ app.js
@@ -1,53 +1,25 @@
-'use strict'
+'use strict';
+
+const config   = require('./config.json')
+const vorpal   = require('vorpal')();
 
 const DigitalOceanHelper = require('./lib/DigitalOceanHelper');
 const DO_KEY    = process.env.DO_KEY;
 const PAGE_SIZE = process.env.PAGE_SIZE || 2;
 const DOHelper  = new DigitalOceanHelper(DO_KEY, PAGE_SIZE);
-const RS        = require('randomstring');
-const config    = require('./config.json');
 
-((DOHelper) => {
-  
-  DOHelper.generateKeys()
-  .then((results) => {
-    return DOHelper.accountAddKey({
-      name: results.uuid,
-      public_key: results.public_key
-    });
-  }, (err) =>{ 
-    console.log(`Error generating keys: ${err}`)
-  })
-  .then((results)=>{
-    /* Results
-    {
-      ssh_key: {
-        id, fingerprint, public_key, name
-      }
-    }
-    */
+const commands = require('./lib/commands')(config, DOHelper);
 
-    let dropletConfig = Object.assign({}, config.droplets, {
-      name: RS.generate(),
-      ssh_keys: [
-       results.body.ssh_key.id
-      ]
-    })
-    return DOHelper.dropletsCreate(dropletConfig)
-    
-  }, (err) => {
-    console.log(err)
-  })
-  .then((results) => {
-    console.log(results.body)
-  }, (err) => {
-    console.log(err);
-  })
-})(DOHelper);
+vorpal.command('list-images', 'List The images')
+.action(commands["list-images"])
 
-// DOHelper.imagesGetAll({
-//   includeAll: true
-// })
-// .then((results) => {
-//   console.dir(results.body)
-// })
+vorpal.command('keys <action> [key]')
+.description('Commands to generate and upload ssh keys to local or digital ocean')
+.option('-l, --local', '<action> applies to local ssh keys')
+.option('-r, --remote', '<action> applies to remote ssh keys')
+.action(commands.keys)
+
+vorpal
+  .delimiter('server-generator')
+  .show();
+
